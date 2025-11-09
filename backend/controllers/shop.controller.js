ceec5ee4a6d19dc4ac18234a35a918e57ce4e36e
Aphichet07 <aphichet254888@gmail.com>
2025-11-09 22:44:11 +0700

    add recommand function

--- backend/controllers/shop.controller.js
+++ backend/controllers/shop.controller.js
@@ -1,27 +1,32 @@
 import shopService from "../services/shop.service.js";
 
 const shopController = {
-    test:  (req, res)=>{
+    test: (req, res) => {
         console.log("Hello from shopController")
-     
-        res.status(200).json({message: shopService.test})
+
+        res.status(200).json({ message: shopService.test })
     },
 
-    getRecommandShop: async (req, res) =>{
-        console.log("Hello from getShop")
+    getRecommandShop: async (req, res) => {
+        try {
+            const shops = await shopService.getShopRecommand()
+            res.status(200).json(shops);
+        } catch (err) {
+            res.status(500).json({ message: "Internal server error" });
+        }
     },
 
-    getShopbyName: async (req, res) =>{
+    getShopbyName: async (req, res) => {
         console.log("Hello from getShopbyName")
     },
-    filterShopAll: async (req, res) =>{
+    filterShopAll: async (req, res) => {
         console.log("Hello from fillterShopAll")
     },
 
-    filterByPrice: async (req, res) =>{
+    filterByPrice: async (req, res) => {
         console.log("Hello from filterByPrice")
     },
-    filterByStar: async (req, res)=>{
+    filterByStar: async (req, res) => {
         console.log("Hello from filterByStar")
     }
 
--- backend/services/shop.service.js
+++ backend/services/shop.service.js
@@ -1,30 +1,40 @@
 import db from '../utils/connectDB.js'
 
 const shopService = {
-    test: ()=>{
+    test: () => {
         return JSON.stringify("Hello from shopService")
     },
 
-    getShopRecommand: async ()=>{
-        // load recommand shop on Home page
-         console.log("Hello from HetAllRecommand")
+    getShopRecommand: async () => {
+        try {
+            const queryText = `
+                SELECT * FROM "coffee_shops"
+                ORDER BY "average_rating" DESC
+                LIMIT 10
+            `
+            const { rows } = await db.query(queryText)
+            return rows;
+        } catch (err) { 
+            console.error("Error in getRecommended service: ", err);
+            throw err;
+        }
     },
 
-    getShopByName: async ()=>{
+    getShopByName: async () => {
         // search name shop on search page
         console.log("Hello from GetShopByName")
     },
 
-    filterShop: async ()=>{
+    filterShop: async () => {
         // filter all of fileter bar
         console.log("Hello from filterShop")
-    }, 
+    },
 
-    filterByPrice: async ()=>{
+    filterByPrice: async () => {
         console.log("Hello from filterByPrice")
     },
 
-    filterByStar: async ()=>{
+    filterByStar: async () => {
         console.log("Hello from filterByStar")
     }
 }
