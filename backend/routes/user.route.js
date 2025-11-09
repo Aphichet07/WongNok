67ed73f55e5e34c0aaa551b34085c2d81c5d71a6
Aphichet07 <aphichet254888@gmail.com>
2025-11-09 18:12:21 +0700

    fix new server file and add user router

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,2 +1,9 @@
-import userrController from "../controllers/user.controller";
+import userController from "../controllers/user.controller";
 
+const userRouter = (route)=> {
+    route.post("/signup", userController.register)
+    route.post("/signin", userController.login)
+    route.post("/findUserByID", userController.finduser)
+}
+
+export default userRouter
